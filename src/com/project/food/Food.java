fa1a0d362572a341dae7ee517825617a9c5368d1
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-22 03:04:14 +0530

    class Menu and class Food
    CSV part not added yet...only added to be seen by all team members

---
+++ src/com/project/food/Food.java
@@ -0,0 +1,42 @@
+package com.project.food;
+
+public class Food implements Comparable<Food>
+{
+    private String dishName;
+    private String dishType;
+
+    public Food(String dishName,String dishType)
+    {
+        this.dishName=dishName;
+        this.dishType=dishType;
+    }
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public void setDishName(String dishName) {
+        this.dishName = dishName;
+    }
+
+    public String getDishType()
+    {
+        return dishType;
+    }
+
+    public void setDishType(String dishType)
+    {
+        this.dishType=dishType;
+    }
+
+    public String toString()
+    {
+        return getDishName()+"("+getDishType()+")";
+    }
+
+    @Override
+    public int compareTo(Food fd)
+    {
+        return getDishName().compareTo(fd.getDishName());
+    }
+}
---
+++ src/com/project/menu/Menu.java
@@ -0,0 +1,307 @@
+package com.project.menu;
+
+import java.util.*;
+import java.sql.*;
+import com.project.food.Food;
+import com.project.util.*;
+
+public class Menu
+{
+    // private String day;
+    // private TreeMap<String,ArrayList<Food>> menuMap=new TreeMap<String,ArrayList<Food>>(); 
+
+    
+    DataBaseUtil databaseutil=new DataBaseUtil();
+
+    public void showMealsofDay(String day)
+    {
+        try
+        {
+            Connection conn=databaseutil.getConnection();
+            Statement st=conn.createStatement(ResultSet.CONCUR_READ_ONLY,ResultSet.TYPE_SCROLL_INSENSITIVE);
+
+            ArrayList<String> meals=QueryUtil.showMealsofDay(day);
+            ArrayList<Food> arrFood=new ArrayList<>(); 
+
+            ArrayList<String> mealStr=new ArrayList<String>();
+            mealStr.add("Breakfast");
+            mealStr.add("Lunch");
+            mealStr.add("Snacks");
+            mealStr.add("Dinner");
+            int k=0;
+
+            System.out.println("Menu for "+day+" is -> ");
+
+            for(String x:meals)
+            {
+                ResultSet rs=st.executeQuery(x);
+                while(rs.next())
+                {
+                    StringTokenizer stk=new StringTokenizer(rs.getString(2),"|");
+
+                    while(stk.hasMoreTokens())
+                    {
+                        StringTokenizer stkFood=new StringTokenizer(stk.nextToken(),",");
+                        while(stkFood.hasMoreTokens())
+                        {
+                            // System.out.print(stkFood.nextToken()+" ");
+                            arrFood.add(new Food(stkFood.nextToken(),stkFood.nextToken()));
+                        }
+                    }
+                }
+                // menuMap.put(mealStr.get(k),arrFood);
+                System.out.print(mealStr.get(k)+" -> ");
+                System.out.println(arrFood);
+                k++;
+                arrFood.clear();
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    //showBreakfast()
+    public void showBreakfast()
+    {
+        System.out.println("-------------------------------------");
+        System.out.println("BreakFast Menu -> ");
+        System.out.println("-------------------------------------");
+
+        try
+        {
+            String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+            for(String str:days)
+            {
+                // System.out.println(str+" ->  "+breakfastMap.get(str));
+                showBreakfastofDay(str);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong");
+        }
+    }
+
+    //showbreakfastday()
+    public void showBreakfastofDay(String day)
+    {
+        try
+        {
+            Connection conn=databaseutil.getConnection();
+            Statement st=conn.createStatement();
+            ResultSet rs=st.executeQuery(QueryUtil.showBreakfastofDay(day));
+
+            ArrayList<Food> arrFood=new ArrayList<Food>();
+
+            while(rs.next())
+            {
+                StringTokenizer stk=new StringTokenizer(rs.getString(2),"|");
+
+                while(stk.hasMoreTokens())
+                {
+                    StringTokenizer stkFood=new StringTokenizer(stk.nextToken(),",");
+                    while(stkFood.hasMoreTokens())
+                    {
+                        arrFood.add(new Food(stkFood.nextToken(),stkFood.nextToken()));
+                    }
+                }
+            }
+
+            System.out.println(day+" -> "+arrFood);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    //showLunch()
+    public void showLunch()
+    {
+        System.out.println("-------------------------------------");
+        System.out.println("Lunch Menu -> ");
+        System.out.println("-------------------------------------");
+
+        try
+        {
+            String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+            for(String str:days)
+            {
+                // System.out.println(str+" ->  "+breakfastMap.get(str));
+                showLunchofDay(str);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong");
+        }
+    }
+
+    //showbreakfastday()
+    public void showLunchofDay(String day)
+    {
+        try
+        {
+            Connection conn=databaseutil.getConnection();
+            Statement st=conn.createStatement();
+            ResultSet rs=st.executeQuery(QueryUtil.showLunchofDay(day));
+            ArrayList<Food> arrFood=new ArrayList<Food>();
+
+            while(rs.next())
+            {
+                StringTokenizer stk=new StringTokenizer(rs.getString(2),"|");
+
+                while(stk.hasMoreTokens())
+                {
+                    StringTokenizer stkFood=new StringTokenizer(stk.nextToken(),",");
+                    while(stkFood.hasMoreTokens())
+                    {
+                        // System.out.print(stkFood.nextToken()+" ");
+                        arrFood.add(new Food(stkFood.nextToken(),stkFood.nextToken()));
+                    }
+                }
+            }
+
+            System.out.println(day+" -> "+arrFood);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    //showBreakfast()
+    public void showSnacks()
+    {
+        System.out.println("-------------------------------------");
+        System.out.println("Snacks Menu -> ");
+        System.out.println("-------------------------------------");
+
+        try
+        {
+            String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+            for(String str:days)
+            {
+                // System.out.println(str+" ->  "+breakfastMap.get(str));
+                showSnacksofDay(str);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong");
+        }
+    }
+
+    //showbreakfastday()
+    public void showSnacksofDay(String day)
+    {
+        try
+        {
+            Connection conn=databaseutil.getConnection();
+            Statement st=conn.createStatement();
+            ResultSet rs=st.executeQuery(QueryUtil.showSnacksofDay(day));
+
+            ArrayList<Food> arrFood=new ArrayList<Food>();
+
+            while(rs.next())
+            {
+                StringTokenizer stk=new StringTokenizer(rs.getString(2),"|");
+
+                while(stk.hasMoreTokens())
+                {
+                    StringTokenizer stkFood=new StringTokenizer(stk.nextToken(),",");
+                    while(stkFood.hasMoreTokens())
+                    {
+                        // System.out.print(stkFood.nextToken()+" ");
+                        arrFood.add(new Food(stkFood.nextToken(),stkFood.nextToken()));
+                    }
+                }
+            }
+
+            System.out.println(day+" -> "+arrFood);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    //showBreakfast()
+    public void showDinner()
+    {
+        System.out.println("-------------------------------------");
+        System.out.println("Dinner Menu -> ");
+        System.out.println("-------------------------------------");
+
+        try
+        {
+            String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+            for(String str:days)
+            {
+                // System.out.println(str+" ->  "+breakfastMap.get(str));
+                showDinnerofDay(str);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong");
+        }
+    }
+
+    //showbreakfastday()
+    public void showDinnerofDay(String day)
+    {
+        try
+        {
+            Connection conn=databaseutil.getConnection();
+            Statement st=conn.createStatement();
+            ResultSet rs=st.executeQuery(QueryUtil.showDinnerofDay(day));
+
+            ArrayList<Food> arrFood=new ArrayList<Food>();
+
+            while(rs.next())
+            {
+                StringTokenizer stk=new StringTokenizer(rs.getString(2),"|");
+
+                while(stk.hasMoreTokens())
+                {
+                    StringTokenizer stkFood=new StringTokenizer(stk.nextToken(),",");
+                    while(stkFood.hasMoreTokens())
+                    {
+                        // System.out.print(stkFood.nextToken()+" ");
+                        arrFood.add(new Food(stkFood.nextToken(),stkFood.nextToken()));
+                    }
+                }
+            }
+
+            System.out.println(day+" -> "+arrFood);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void showMenu()
+    {
+        System.out.println("-------------------------------------");
+        System.out.println("Menu -> ");
+        System.out.println("-------------------------------------");
+
+        try
+        {
+            showBreakfast();
+            showLunch();
+            showSnacks();
+            showDinner();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something wrong");
+        }
+    }
+}
